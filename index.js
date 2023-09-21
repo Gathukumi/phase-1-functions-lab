c9cfaf0aa7798afd4b8dbb6990d7190545bf2431
Gathukumi <gathukumidavid001@gmail.com>
2023-09-22 00:25:50 +0300

    complted lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function distanceFromHqInBlocks(blockNumber) {
-    const hqLocation = 42; // mtaa iko on the 42nd block
-    return Math.abs(blockNumber - hqLocation); // distance in blocks from hq is gonna be the block you are at - hq(42)
+    const hqLocation = 42; 
+    return Math.abs(blockNumber - hqLocation); 
   }
   function distanceFromHqInFeet(blockNumber) {
     const blocks = distanceFromHqInBlocks(blockNumber);
@@ -8,9 +8,9 @@ function distanceFromHqInBlocks(blockNumber) {
     return blocks * feetPerBlock;
   }
   function distanceTravelledInFeet(start, destination) {
-    const feetInBlock = 264; // From canvas we saw 1 block = 264 feet
-    const distanceInBlocks = Math.abs(destination - start); // to get distance between travelled blocks
-    return distanceInBlocks * feetInBlock; // distance in feet btwn blocks is gonna be distance in blocks * 264
+    const feetInBlock = 264; 
+    const distanceInBlocks = Math.abs(destination - start); 
+    return distanceInBlocks * feetInBlock; 
   }
   function calculatesFarePrice(startBlock, endBlock) {
       const feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
@@ -19,14 +19,14 @@ function distanceFromHqInBlocks(blockNumber) {
   function calculatesFarePrice(startBlock, endBlock) {
       const feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
       let calculatedFare;
-      if (feetTravelled <= 400) { //if feet travelled is less that 400 feet
-          return 0; //fare is free
-          } else if (feetTravelled > 400 && feetTravelled <= 2000) { //if feet is more than 400 but less than 2000 feet
-          return (feetTravelled - 400) * 0.02; // fare is gonna be feet travelled * 2 cents
-          } else if (feetTravelled > 2000 && feetTravelled <= 2500) { // if feet is more than 2000 feet but less than 2500
-          return 25 ; // fare is gonna be 25 dollars
+      if (feetTravelled <= 400) { 
+          return 0; 
+          } else if (feetTravelled > 400 && feetTravelled <= 2000) { 
+          return (feetTravelled - 400) * 0.02; 
+          } else if (feetTravelled > 2000 && feetTravelled <= 2500) { 
+          return 25 ; 
           } else {
-        calculatedFare = "cannot travel that far"; // feet is more than 2500, not applicable 
+        calculatedFare = "cannot travel that far"; 
       }
        return calculatedFare;
   }
